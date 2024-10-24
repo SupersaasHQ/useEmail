a66287cfe125b1eb5f578896566932d943ddaf64
Johann Schopplich <mail@johannschopplich.com>
2024-10-24 15:13:11 +0200

    chore: separate build chunks for services

---
+++ build.config.ts
@@ -0,0 +1,16 @@
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+  entries: [
+    "src/index",
+    "src/services/mailgun",
+    "src/services/plunk",
+    "src/services/postmark",
+    "src/services/sendgrid",
+  ],
+  clean: false,
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+  },
+});
