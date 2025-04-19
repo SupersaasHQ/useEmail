43af4b6a5bbb86380cc4b4c145b7101413a50f49
Mark Bruderer <markbruderer123@gmail.com>
2025-04-19 22:33:10 +0200

    feat: add mock service.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { MailgunService } from "./services/mailgun";
 import { ZeptoMailService } from "./services/zeptomail";
 import type { EmailProvider } from "./types/email-options";
 import type { EmailService } from "./types/email-service";
+import { MockService } from "./services/mock";
 
 /**
  * Factory function to get the email service based on the provider
@@ -32,6 +33,9 @@ export function useEmail(provider: EmailProvider): EmailService {
     }
     case "zeptomail": {
       return new ZeptoMailService();
+    }
+    case "mock": {
+      return MockService();
     }
     default: {
       throw new Error(`Unsupported email provider: ${provider}`);
